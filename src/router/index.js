cbebb0fc88293e5ebf213ca2cd681c07dd7266c7
Mrfneto <mrfneto@gmail.com>
2022-09-25 13:23:03 -0300

    Navigation Guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../components/HomeView.vue";
+import { getUserState } from "../firebase/useFirebase";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,13 +9,28 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/login",
       name: "login",
       component: () => import("../components/LoginView.vue"),
+      meta: { requiresUnauth: true },
     },
   ],
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const requiresUnauth = to.matched.some(
+    (record) => record.meta.requiresUnauth
+  );
+
+  const isAutenticated = await getUserState();
+
+  if (requiresAuth && !isAutenticated) next({ name: "login" });
+  else if (requiresUnauth && isAutenticated) next({ name: "home" });
+  else next();
 });
 
 export default router;
