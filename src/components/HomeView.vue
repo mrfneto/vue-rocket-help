0e6a4c9d73b2f732628a21824ad15ba1c9864597
Mrfneto <mrfneto@gmail.com>
2022-09-26 14:51:32 -0300

    Home Filter Layout

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
-import LogoutIcon from "./icons/LogoutIcon.vue";
-import { logoutUser } from "../firebase/useFirebase";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { logoutUser } from "../firebase/useFirebase";
 import Logo from "./shared/Logo.vue";
+import LogoutIcon from "./icons/LogoutIcon.vue";
 
 const router = useRouter();
+
+const active = ref(true);
 
 const logout = async () => {
   try {
@@ -25,7 +28,26 @@ const logout = async () => {
       </button>
     </header>
     <main class="flex-1 py-8 px-4">
-      <h1 class="text-3xl font-bold underline mb-4">Home Page</h1>
+      <section class="flex items-center justify-between mb-6">
+        <h2>Solicitações</h2>
+        <p class="text-sm text-zinc-400">0</p>
+      </section>
+      <section class="flex items-center space-x-2">
+        <button
+          class="btn w-full text-sm btn--secondary"
+          :class="active ? '' : 'btn__outline--secondary'"
+          @click="active = false"
+        >
+          Em andamento
+        </button>
+        <button
+          class="btn btn--secondary w-full text-sm"
+          :class="active ? 'btn__outline--primary' : ''"
+          @click="active = true"
+        >
+          Finalizado
+        </button>
+      </section>
     </main>
     <footer>footer</footer>
   </div>
